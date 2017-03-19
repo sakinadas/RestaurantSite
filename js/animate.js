bafad56ca856ee29d27c5a7210ebd572bb61b999
SD <sakina.das@gmail.com>
2017-03-19 14:37:51 -0700

    Site Details

---
+++ js/animate.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+	$(window).bind('scroll', function(){
+		 var navHeight = $(window).height() - 61;
+		//alert(navHeight);
+		if($(window).scrollTop() > navHeight){
+			$('navbar').addClass('fixed');
+		}
+		else{
+			$('navbar').removeClass('fixed');
+		}
+		
+	});
+});
+
+$(window).on('beforeunload', function(){
+    $(window).scrollTop(0);
+});
